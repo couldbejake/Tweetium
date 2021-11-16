dc7bcdcbca16fcff4d0e308e65af7fbe3087fd06
webclones <couldbejake@gmail.com>
2021-11-16 12:34:35 +0000

    hi

--- tweetium.js
+++ tweetium.js
@@ -1,1 +1,17 @@
-console.log('bannas')
+
+var url = 'http://graph.facebook.com/517267866/?fields=picture';
+
+http.get(url, function(res){
+    var body = '';
+
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+
+    res.on('end', function(){
+        var fbResponse = JSON.parse(body);
+        console.log("Got a response: ", fbResponse.picture);
+    });
+}).on('error', function(e){
+      console.log("Got an error: ", e);
+});
